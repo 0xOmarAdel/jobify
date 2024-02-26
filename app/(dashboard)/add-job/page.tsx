3c2bd5e6beed097492731e1278651fc57e2c7804
Heisenberg <omaradelawad20013@gmail.com>
2024-02-26 11:45:11 +0200

    created add job form

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,5 +1,11 @@
+import CreateJobForm from "@/components/CreateJobForm";
+
 const AddJobPage = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <CreateJobForm />
+    </>
+  );
 };
 
 export default AddJobPage;
